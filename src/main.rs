27eccc5915c4fdd7d41bcb2dc751480950efff13
Slackers <severin.gebesmair@gmx.at>
2023-07-26 13:51:03 +0200

    initial commit (working renderer)

---
+++ src/main.rs
@@ -0,0 +1,167 @@
+use glam::{Mat4, Vec3};
+use math::{random_vec_in_hemisphere, Camera, Color, Material};
+
+use crate::math::{Ray, Renderable};
+use show_image::create_window;
+
+mod math;
+
+// the following are options
+const SCREEN_HEIGHT: u32 = 1080;
+const SCREEN_WIDTH: u32 = 1920;
+const SUN_DIR: Vec3 = Vec3::new(0.1, 1.0, 0.3);
+const BOUNCE_AMOUNT: u32 = 70;
+const SAMPLES_PER_PIXEL: u32 = 100;
+const SKY_COL: Color = Color {
+    r: 0.5,
+    g: 0.7,
+    b: 1.0,
+};
+
+// the following are not to be tweaked
+const PIXEL_SIZE: f32 = 1.0 / SCREEN_WIDTH as f32;
+const PIXEL_OFF_HEIGHT: f32 = PIXEL_SIZE * (SCREEN_HEIGHT as f32 / 2.0);
+
+type Scene = Vec<Box<dyn Renderable>>;
+
+#[show_image::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut tri = math::Tri {
+        a: Vec3::new(0.0, 1.0, 1.5),
+        b: Vec3::new(0.5, 0.0, 1.5),
+        c: Vec3::new(-0.5, 0.0, 1.5),
+        material: Material {
+            color: Color {
+                r: 0.5,
+                g: 0.0,
+                b: 0.5,
+            },
+            metalness: 0.2,
+        },
+    };
+
+    let mut sphere = math::Sphere {
+        pos: Vec3::new(0.0, 1.0, 10.0),
+        rad: 1.0,
+        material: Material {
+            color: Color {
+                r: 1.0,
+                g: 1.0,
+                b: 0.0,
+            },
+            metalness: 0.5,
+        },
+    };
+
+    let mut plane = math::Plane {
+        pos: Vec3::new(0.0, 0.0, 0.0),
+        norm: Vec3::new(0.0, 1.0, 0.0),
+        material: Material {
+            color: Color {
+                r: 1.0,
+                g: 0.0,
+                b: 0.0,
+            },
+            metalness: 0.0,
+        },
+    };
+
+    let camera = Camera {
+        pos: Vec3::new(0.0, 1.0, 0.0),
+        dir: Vec3::new(0.0, 0.0, 1.0),
+    };
+
+    let view_matrix = Mat4::look_to_lh(camera.pos, camera.dir, Vec3::Y);
+
+    tri.to_homogeneous(view_matrix);
+    sphere.to_homogeneous(view_matrix);
+    plane.to_homogeneous(view_matrix);
+
+    use image::{Rgb, RgbImage};
+
+    let scene: Scene = vec![Box::new(sphere), Box::new(plane)];
+
+    let mut img = RgbImage::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let t_start = std::time::Instant::now();
+    for y in 0..SCREEN_HEIGHT {
+        println!("{}% done", (y as f32 / SCREEN_HEIGHT as f32) * 100.0);
+        for x in 0..SCREEN_WIDTH {
+            let mut pixel_col = SKY_COL;
+            for s in 0..SAMPLES_PER_PIXEL {
+                let r = Ray {
+                    pos: Vec3::ZERO,
+                    dir: Vec3::new(
+                        -0.5 + (PIXEL_SIZE * x as f32) + rand::random::<f32>() * PIXEL_SIZE,
+                        PIXEL_OFF_HEIGHT - (PIXEL_SIZE * y as f32)
+                            + rand::random::<f32>() * PIXEL_SIZE,
+                        1.0,
+                    ),
+                };
+                pixel_col = pixel_col + cast_ray_recursive(&scene, r, 0);
+            }
+            let ratio = 1.0 / SAMPLES_PER_PIXEL as f32;
+            pixel_col = pixel_col * ratio;
+            img.put_pixel(
+                x,
+                y,
+                Rgb([
+                    (255.0 * pixel_col.r.sqrt()) as u8,
+                    (255.0 * pixel_col.g.sqrt()) as u8,
+                    (255.0 * pixel_col.b.sqrt()) as u8,
+                ]),
+            );
+        }
+    }
+    println!("it took {:?} to render", t_start.elapsed());
+
+    let window = create_window("image", Default::default())?;
+    window.set_image("image-001", img.clone())?;
+
+    for event in window.event_channel()? {
+        if let show_image::event::WindowEvent::KeyboardInput(event) = event {
+            if event.input.key_code == Some(show_image::event::VirtualKeyCode::Escape)
+                && event.input.state.is_pressed()
+            {
+                break;
+            }
+        }
+    }
+    img.save("rendered_image.png")?;
+
+    Ok(())
+}
+
+fn cast_ray_recursive(scene: &Scene, ray: Ray, d: u32) -> Color {
+    if d == BOUNCE_AMOUNT {
+        return Color::BLACK;
+    }
+
+    match find_closest(scene, ray) {
+        Some((t, n, mat)) => {
+            let res_p = ray.pos + ray.dir * t;
+            let target = res_p + n + random_vec_in_hemisphere();
+            return cast_ray_recursive(
+                scene,
+                Ray {
+                    pos: res_p,
+                    dir: target - res_p,
+                },
+                d + 1,
+            ) * 0.5;
+        }
+        None => {
+            let unit_dir = ray.dir.normalize();
+            let t = 0.5 * (unit_dir.y + 1.0);
+            return Color::WHITE * (1.0 - t) + SKY_COL * t;
+        }
+    }
+}
+
+fn find_closest(scene: &Scene, ray: Ray) -> Option<(f32, Vec3, Material)> {
+    scene
+        .iter()
+        .filter_map(|i| i.intersect(ray))
+        .filter_map(|i| if i.0 < 0.001 { None } else { Some(i) })
+        .min_by(|a, b| a.0.total_cmp(&b.0))
+}
---
+++ src/math.rs
@@ -0,0 +1,243 @@
+use glam::{Mat4, Vec3, Vec4, Vec4Swizzles};
+
+pub const EPSILON: f32 = 0.0001;
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+impl Color {
+    pub const BLACK: Self = Color {
+        r: 0.0,
+        g: 0.0,
+        b: 0.0,
+    };
+    pub const WHITE: Self = Color {
+        r: 1.0,
+        g: 1.0,
+        b: 1.0,
+    };
+}
+
+impl std::ops::Mul<f32> for Color {
+    type Output = Color;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            r: self.r * rhs,
+            g: self.g * rhs,
+            b: self.b * rhs,
+        }
+    }
+}
+impl std::ops::Add<Color> for Color {
+    type Output = Color;
+    fn add(self, rhs: Color) -> Self::Output {
+        Self {
+            r: self.r + rhs.r,
+            g: self.g + rhs.g,
+            b: self.b + rhs.b,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Material {
+    pub color: Color,
+    pub metalness: f32,
+}
+
+pub trait Renderable {
+    fn intersect(&self, ray: Ray) -> Option<(f32, Vec3, Material)>;
+    fn to_homogeneous(&mut self, view_mat: Mat4);
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Camera {
+    pub pos: Vec3,
+    pub dir: Vec3,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+pub struct Ray {
+    pub pos: Vec3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn normalize(&mut self) {
+        self.dir = self.dir.normalize();
+    }
+    pub fn reposition(mut self, t: f32) -> Self {
+        self.normalize();
+        self.pos = self.pos + self.dir * t;
+        self
+    }
+
+    pub fn mirror(mut self, normal: Vec3) -> Self {
+        self.normalize();
+        let normal = normal.normalize();
+        //self.dir = -self.dir;
+        self.dir = self.dir - 2.0 * (self.dir.dot(normal)) * normal;
+        self
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Tri {
+    pub a: Vec3,
+    pub b: Vec3,
+    pub c: Vec3,
+    pub material: Material,
+}
+
+impl Renderable for Tri {
+    fn intersect(&self, mut ray: Ray) -> Option<(f32, Vec3, Material)> {
+        ray.dir = ray.dir.normalize();
+        let edge1 = self.b - self.a;
+        let edge2 = self.c - self.a;
+
+        let h = ray.dir.cross(edge2);
+        let a = edge1.dot(h);
+
+        if a > -EPSILON && a < EPSILON {
+            return None;
+        }
+
+        let f = 1.0 / a;
+        let s = ray.pos - self.a;
+        let u = f * s.dot(h);
+
+        if u < 0.0 || u > 1.0 {
+            return None;
+        }
+
+        let q = s.cross(edge1);
+        let v = f * ray.dir.dot(q);
+
+        if v < 0.0 || u + v > 1.0 {
+            return None;
+        }
+
+        let t = f * edge2.dot(q);
+
+        if t > EPSILON {
+            return Some((t, edge1.cross(edge2), self.material));
+        }
+
+        None
+    }
+
+    fn to_homogeneous(&mut self, view_mat: Mat4) {
+        self.a = (view_mat * Vec4::from((self.a, 1.0))).xyz();
+        self.b = (view_mat * Vec4::from((self.b, 1.0))).xyz();
+        self.c = (view_mat * Vec4::from((self.c, 1.0))).xyz();
+    }
+}
+
+pub struct Sphere {
+    pub pos: Vec3,
+    pub rad: f32,
+    pub material: Material,
+}
+
+impl Renderable for Sphere {
+    fn intersect(&self, mut ray: Ray) -> Option<(f32, Vec3, Material)> {
+        ray.dir = ray.dir.normalize();
+        let l_vec = self.pos - ray.pos;
+        let l_l = l_vec.length();
+        let tc = l_vec.dot(ray.dir);
+
+        if tc < 0.0 {
+            return None;
+        }
+
+        let d2 = ((tc * tc) - (l_l * l_l)).abs();
+
+        let rad2 = self.rad * self.rad;
+        if d2 > rad2 {
+            return None;
+        }
+
+        let t1c = (rad2 - d2).sqrt();
+
+        let t = tc - t1c;
+
+        let p = ray.pos + ray.dir * t;
+
+        Some((t, p - self.pos, self.material))
+    }
+
+    fn to_homogeneous(&mut self, view_mat: Mat4) {
+        self.pos = (view_mat * Vec4::from((self.pos, 1.0))).xyz();
+    }
+}
+
+pub struct Plane {
+    pub pos: Vec3,
+    pub norm: Vec3,
+    pub material: Material,
+}
+
+impl Renderable for Plane {
+    fn intersect(&self, ray: Ray) -> Option<(f32, Vec3, Material)> {
+        let denom = self.norm.dot(ray.dir);
+        if denom.abs() > EPSILON {
+            let t = (self.pos - ray.pos).dot(self.norm) / denom;
+            if t >= 0.0 {
+                return Some((t - EPSILON, self.norm, self.material));
+            }
+        }
+        None
+    }
+    fn to_homogeneous(&mut self, view_mat: Mat4) {
+        self.pos = (view_mat * Vec4::from((self.pos, 1.0))).xyz();
+    }
+}
+
+pub fn random_vec(min: f32, max: f32) -> Vec3 {
+    let diff = max - min;
+    Vec3 {
+        x: (rand::random::<f32>() * diff) + min,
+        y: (rand::random::<f32>() * diff) + min,
+        z: (rand::random::<f32>() * diff) + min,
+    }
+}
+
+pub fn random_vec_in_hemisphere(normal: Vec3) -> Vec3 {
+    loop {
+        let v = random_vec(-1.0, 1.0);
+        if v.length_squared() >= 1.0 {
+            continue;
+        }
+        if v.dot(rhs)
+        return v.normalize();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use glam::Vec3;
+
+    use super::Ray;
+
+    #[test]
+    fn ray_mirroring() {
+        let mut ray = Ray {
+            pos: Vec3::new(-3.0, 3.0, 0.0),
+            dir: Vec3::new(1.0, -1.0, 0.0),
+        };
+        ray.normalize();
+        let normal = Vec3::new(0.0, 1.0, 0.0);
+
+        assert_eq!(
+            ray.mirror(normal),
+            Ray {
+                pos: Vec3::new(-3.0, 3.0, 0.0),
+                dir: Vec3::new(1.0, -1.0, 0.0),
+            }
+        );
+    }
+}
